940f75fb5ae7e95ea6a5b7a99847586e5a3265fe
mevlanaayas <mevlanaayas@gmail.com>
2019-02-15 01:26:50 +0300

    added dark theme

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
         <transition name="router-anim" enter-active-class="animated fadeIn">
             <router-view></router-view>
         </transition>
+        <el-button class="right-bottom-btn" @click="changeTheme">
+            Change theme
+        </el-button>
     </div>
 
 </template>
@@ -11,6 +14,15 @@
 
     export default {
         name: 'app',
+        methods: {
+            changeTheme() {
+                if(document.body.className === 'dark') {
+                    document.body.className = 'white';
+                }else {
+                    document.body.className = 'dark';
+                }
+            }
+        }
     }
 
 
@@ -23,5 +35,37 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         margin-top: 60px;
+    }
+    body.white {
+
+    }
+    body.dark {
+        background-color: #2c2c2c !important;
+        color: white;
+    }
+    body.dark .el-form-item__label {
+        color: whitesmoke;
+    }
+    body.dark .el-input__inner {
+        background-color: grey;
+        color: #2c2c2c;
+    }
+    body.dark .el-input__icon {
+        color: #2c2c2c;
+    }
+    body.dark .el-button--primary {
+        background-color: grey;
+        border-color: whitesmoke;
+        color: #2c2c2c;
+    }
+    body.dark .el-picker-panel {
+        background-color: grey;
+        color: whitesmoke;
+    }
+    .right-bottom-btn {
+        position: absolute;
+        z-index: 11;
+        right: 0;
+        bottom: 0;
     }
 </style>
